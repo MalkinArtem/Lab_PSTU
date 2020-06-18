021bb605f75bf7842aba5d7ee73bc231c744bf93
Малкин Артём АСУбз <65707973+MalkinArtem@users.noreply.github.com>
2020-06-18 16:31:59 +0500

    Add files via upload

---
+++ 1. Знакомство с С++/Лабораторная работа №1.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "rus");
+
+	int n, m;
+
+	cout << "Введите n: ";
+
+	cin >> n;
+
+	cout << "Введите m: ";
+
+	cin >> m;
+
+	int res1 = ++n + m;
+
+	cout << "n++ + m = " << res1 << endl;
+
+	bool res2 = --m > m;
+
+	cout << "m-- > m = " << res2 << endl;
+
+	bool res3 = --n > m;
+
+	cout << "n-- > m = " << res3 << endl;
+
+	system("pause");
+
+	return 0;
+
+}
+
---
+++ 1. Знакомство с С++/Лабораторная работа №1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+
+{
+
+	float a = 1000;
+
+	float b = 0.0001;
+
+	float res;
+
+	res = ((a * a + 2 * a * b + b * b) - (a * a + 2 * a * b)) / (b * b);
+
+	cout << "Float = " << res << endl;
+
+	double a1 = 1000;
+
+	double b1 = 0.0001;
+
+	double res1;
+
+	res1 = ((a1 * a1 + 2 * a1 * b1 + b1 * b1) - (a1 * a1 + 2 * a1 * b1)) / (b1 * b1);
+
+	cout << "Double = " << res1 << endl;
+
+	system("pause");
+
+	return 0;
+
+
+}
